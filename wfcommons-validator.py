9f9be9685e3375ed8b98398c872ea9d42cc89013
Rafael Ferreira da Silva <rafsilva@isi.edu>
2021-03-07 12:01:47 -0800

    preparing release 1.1

--- wfcommons-validator.py
+++ wfcommons-validator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2020 The WorkflowHub Team.
+# Copyright (c) 2020-2021 The WfCommons Team.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
