f992dee7d79490ce1058b17dd42ac009d0f529ac
Henri Casanova <henric@hawaii.edu>
2026-08-06 09:33:12 -1000

    Removed dead code from metrics-computation script

--- tools/wfcommons-add-metrics-to-instance.py
+++ tools/wfcommons-add-metrics-to-instance.py
@@ -10,36 +10,6 @@ from collections import defaultdict
 from collections import Counter, defaultdict, deque
 
 
-
-class Graph:
-    """Graph data structure represented as an adjacency list using a dictionary."""
-
-    def __init__(self):
-        """Initialize the graph."""
-        self.adj_dict = defaultdict(list)
-
-    def add_node(self, node):
-        """
-        Add a node to the graph.
-
-        Args:
-            node (any): The node to add to the graph
-        """
-        self.adj_dict[node] = []
-
-    def add_edge(self, u, v):
-        """
-        Add a directed edge to the graph. Prevents adding duplicate directed edges.
-
-        Args:
-            u (any): The node to start from
-            v (any): The node to point to
-        """
-        if v not in self.adj_dict[u]:
-            self.adj_dict[u].append(v)
-
-
-
 def add_specification_metrics(data):
     if "metrics" not in data["workflow"]["specification"]:
         data["workflow"]["specification"]["metrics"] = {}
